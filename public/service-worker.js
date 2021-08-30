458767ae8f08108ac3dcd1b15adb2e55892c0539
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-08-29 21:42:26 -0700

    Added manifest.json into service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,7 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./js/idb.js",
   "./js/index.js",
+  "./manifest.json",
   "./css/styles.css",
   "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
